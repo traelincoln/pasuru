b4a9bdabb290c90d03b17f3f4be7888474a8d770
Trevour L Chidoti <63824807+traelincoln@users.noreply.github.com>
2022-11-20 12:30:37 +0200

    pasuru website initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import * as bootstrap from 'bootstrap';
+
+console.log("hello world!");
+
+const confirm_btn = document.getElementById("btn-confirm");
+
+confirm_btn.addEventListener("click", () => window.alert("Okay Cool!"))
